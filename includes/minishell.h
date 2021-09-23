9dd7aa06002dbab80f7a73b285959362dd39b16b
keulee <klee8903@gmail.com>
2021-09-24 00:09:38 +0200

    parsing..

--- includes/minishell.h
+++ includes/minishell.h
@@ -14,9 +14,23 @@
 
 # include "../libft/libft.h"
 
+# define TEXT 0
+# define SPACE 1
+# define SINQ 2
+# define DOUQ 3
+# define BSLA 4
+# define DOLR 5
+# define PIPE 6
+# define DPIP 7
+# define SEMC 8
+# define DSEM 9
+# define RIGHT 10
+# define DRIGHT 11
+# define LEFT 12
+
 typedef struct s_pars {
 	int				type;
-	char 			*content;
+	char 			*str;
 	struct s_pars	*prev;
 	struct s_pars	*next;
 } t_pars;

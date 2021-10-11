1f8191b396d5a832bd1ac8b1fad45c8feea03ee1
keulee <klee8903@gmail.com>
2021-10-11 20:42:16 +0200

    operator word parse added

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -37,6 +37,34 @@ int	is_quotes_pair(char *line, int *index, char pair)
 	return (1);
 }
 
+void	operation_word(t_cmd **cmd, char *line, int *index)
+{
+	if (!ft_strncmp(&line[*index], "<", 1))
+	{
+		if (line[(*index) + 1] == '<')
+		{
+			insert_node(cmd, DLEFT, ft_strdup("<<"));
+			(*index)++;
+		}
+		else
+			insert_node(cmd, LEFT, ft_strdup("<"));
+	}
+	else if (!ft_strncmp(&line[*index], ">", 1))
+	{
+		if (line[(*index) + 1] == '>')
+		{
+			insert_node(cmd, DRIGHT, ft_strdup(">>"));
+			(*index)++;
+		}
+		else
+			insert_node(cmd, RIGHT, ft_strdup(">"));
+	}
+	else if (!ft_strncmp(&line[*index], "|", 1))
+		insert_node(cmd, PIPE, ft_strdup("|"));
+	else if (!ft_strncmp(&line[*index], "$", 1))
+		insert_node(cmd, DOLR, ft_strdup("$"));
+}
+
 int ft_parsing(char *line, t_cmd **cmd)
 {
 	int i;
@@ -50,17 +78,19 @@ int ft_parsing(char *line, t_cmd **cmd)
 		if (line[i] == '\"' || line[i] == '\'')
 		{
 			if (parsing_quotes(line, &i, cmd))
-			// if (is_quotes_pair(line, &i))
 			{
 				ft_putendl_fd("Error", 1); /* 임시 message */
 				return (1);
 			}
-			/* parsing string with quotes*/
 		}
 		else if (line[i] == ';' || line[i] == '\\')
 		{
 			ft_putendl_fd("Syntax error", 1); /* 임시 message */
 				return (1);
+		}
+		else if (line[i] == '<' || line[i] == '>' || line[i] == '|' || line[i] == '$')
+		{
+			operation_word(cmd, line, &i);
 		}
 		else
 		{

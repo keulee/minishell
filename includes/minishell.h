72c67814dc318c285bd17059a7e44841df262fb8
keulee <klee8903@gmail.com>
2021-11-24 23:45:02 +0100

    h file modified

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: keulee <keulee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 00:59:39 by keulee            #+#    #+#             */
-/*   Updated: 2021/11/23 01:47:11 by keulee           ###   ########.fr       */
+/*   Updated: 2021/11/24 23:44:43 by keulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,7 +131,6 @@ int				main(int ac, char **av, char **env);
 /* parsing.c */
 int				parsing_process(char *str, t_cmd **cmd, int *i);
 void			insert_nospace_flag(t_cmd **cmd);
-// char			*remove_quote(char *line);
 char			*remove_quote(char *line, int *flag);
 int				ft_parsing(char *line, t_cmd **cmd);
 
